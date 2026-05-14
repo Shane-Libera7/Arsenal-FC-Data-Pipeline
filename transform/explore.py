72d2b72b853e7162ccc33cdb6e32bc5765c5426f
Shane <shanelibera7@gmail.com>
2026-05-14 10:49:19 +0100

    feat: transform data

--- transform/explore.py
+++ transform/explore.py
@@ -1,16 +1,22 @@
 import pandas as pd 
 
-team_stats = pd.read_csv("data/team_stats_raw.csv", header=[0,1], index_col=[0,1,2])
+team_stats_fbref = pd.read_csv("data/team_stats_fbref_raw.csv", header=[0,1], index_col=[0,1,2])
+team_stats_understat = pd.read_csv("data/team_stats_understat_raw.csv")
 shooting_stats = pd.read_csv("data/shooting_stats_raw.csv", header=[0,1], index_col=[0,1,2])
-player_stats = pd.read_csv("data/player_stats_raw.csv", header=[0,1], index_col=[0,1,2])
+player_stats = pd.read_csv("data/player_stats_raw.csv")
 
 
-print(("-- TEAM STATS--"))
-print(team_stats.shape)
-print(team_stats.dtypes)
-print(team_stats.isnull().sum())
+print(("-- FBREF TEAM STATS--"))
+print(team_stats_fbref.shape)
+print(team_stats_fbref.dtypes)
+print(team_stats_fbref.isnull().sum())
 
-print(("-- SHOOTING STATS--"))
+print("-- UNDERSTAT TEAM STATS--")
+print(team_stats_understat.shape)
+print(team_stats_understat.dtypes)
+print(team_stats_understat.isnull().sum())
+
+print(("--SHOOTING STATS--"))
 print(shooting_stats.shape)
 print(shooting_stats.dtypes)
 print(shooting_stats.isnull().sum())
@@ -23,6 +29,3 @@ print(player_stats.isnull().sum())
 
 
 
-print(player_stats.index.get_level_values(2).unique())
-
-print(shooting_stats.columns.tolist())
--- transform/transform.py
+++ transform/transform.py
@@ -2,26 +2,51 @@ import pandas as pd
 
 #Load Raw data 
 
-team_stats = pd.read_csv("data/team_stats_raw.csv", header=[0,1], index_col=[0,1,2])
+team_stats_fbref = pd.read_csv("data/team_stats_fbref_raw.csv", header=[0,1], index_col=[0,1,2])
+team_stats_understat = pd.read_csv("data/team_stats_understat_raw.csv")
 shooting_stats = pd.read_csv("data/shooting_stats_raw.csv", header=[0,1], index_col=[0,1,2])
-player_stats = pd.read_csv("data/player_stats_raw.csv", header=[0,1], index_col=[0,1,2])
+player_stats = pd.read_csv("data/player_stats_raw.csv")
 
 
-#Clean Team Stats 
+#Clean FBref Team Stats 
+big_6 = ['Arsenal', 'Chelsea', 'Liverpool', 'Manchester Utd', 'Manchester United', 'Manchester City', 'Tottenham Hotspur', 'Tottenham']
 
-team_stats = team_stats.drop(columns=[('url', 'Unnamed: 23_level_1')])
+team_stats_fbref = team_stats_fbref.drop(columns=[('url', 'Unnamed: 23_level_1')])
+
+
+
+#Filter for just big 6 teams 
+team_stats_fbref = team_stats_fbref[team_stats_fbref.index.get_level_values(2).isin(big_6)]
 
 #Rename Columns to clean names 
-team_stats.columns = ['players_used', 'avg_age', 'possession',
+team_stats_fbref.columns = ['players_used', 'avg_age', 'possession',
     'matches_played', 'starts', 'minutes', '90s',
     'goals', 'assists', 'goals_assists', 'goals_no_pen',
     'penalties_scored', 'penalties_att', 'yellow_cards', 'red_cards',
     'goals_per90', 'assists_per90', 'goals_assists_per90',
     'goals_no_pen_per90', 'goals_assists_no_pen_per90']
 
-#Clean Shooting Stats 
 
+#Clean Understat Team stats
+team_stats_understat = team_stats_understat.drop(columns=['league_id', 'season_id', 'game_id', 'home_team_id', 'away_team_id', 'date'])
+
+
+#Filter for Big 6 teams 
+team_stats_understat = team_stats_understat[
+    (team_stats_understat['home_team'].isin(big_6)) |
+    (team_stats_understat['away_team'].isin(big_6))
+]
+
+
+
+
+
+
+#Clean Shooting Stats 
 shooting_stats = shooting_stats.drop(columns=[('url', 'Unnamed: 15_level_1')])
+
+#Filter for big 6 
+shooting_stats = shooting_stats[shooting_stats.index.get_level_values(2).isin(big_6)]
 
 #Rename columns 
 shooting_stats.columns = ['players_used', '90s', 'goals', 'shots', 'shots_on_target',
@@ -32,47 +57,28 @@ shooting_stats.columns = ['players_used', '90s', 'goals', 'shots', 'shots_on_tar
 
 #Clean Player stats 
 
-#Drop Columns not needed
-player_stats = player_stats.drop(columns=['nation', 'born', 'Unnamed: 3_level_0'])
-
-#Sort index 
-player_stats = player_stats.sort_index()
-
-#Drop nulls 
-player_stats = player_stats.dropna()
-
 #Filter for just Arsenal players 
-player_stats = player_stats[player_stats.index.get_level_values(2) == 'Arsenal']
+player_stats = player_stats[player_stats['team'] == 'Arsenal']
+
+#Drop Columns not needed
+player_stats = player_stats.drop(columns=['league_id', 'season_id', 'team_id', 'player_id'])
 
 
-#Rename columns 
-player_stats.columns = ['position', 'age',
-    'matches_played', 'starts', 'minutes', '90s',
-    'goals', 'assists', 'goals_assists', 'goals_no_pen',
-    'penalties_scored', 'penalties_att', 'yellow_cards', 'red_cards',
-    'goals_per90', 'assists_per90', 'goals_assists_per90',
-    'goals_no_pen_per90', 'goals_assists_no_pen_per90']
-
-
-
-
-#Fix Data Types 
-
-for col in ['matches_played', 'starts', 'minutes', 'yellow_cards', 'red_cards']:
-    player_stats[col] = player_stats[col].astype(int)
 
 
 
 
 #Save Clean Data
 
-team_stats.to_csv("data/team_stats_clean.csv", index=True)
+team_stats_fbref.to_csv("data/team_stats_clean.csv", index=True)
+team_stats_understat.to_csv("data/team_stats_understat_clean.csv", index=False)
 shooting_stats.to_csv("data/shooting_stats_clean.csv", index=True)
-player_stats.to_csv("data/player_stats_clean.csv", index=True)
+player_stats.to_csv("data/player_stats_clean.csv", index=False)
 
 
 
 print("Tranformation complete")
-print(f"Team Stats: {team_stats.shape}")
+print(f"Team FBref Stats: {team_stats_fbref.shape}")
+print(f"Understat team Stats: {team_stats_understat.shape}")
 print(f"Shooting Stats: {shooting_stats.shape}")
 print(f"Player Stats: {player_stats.shape}")
